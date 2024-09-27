00cfb306c4d74749fea55385473b84db723abfff
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-27 14:30:14 +1200

    Use memoization technique and struct to avoid calculating every time the expensive function is called

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32{
-    println!("Calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
+// fn simulated_expensive_calculation(intensity: u32) -> u32{
+//     println!("Calculating slowly...");
+//     thread::sleep(Duration::from_secs(2));
+//     intensity
+// }
 
 struct Cacher<T>
 where 
@@ -15,28 +15,54 @@ where
     value: Option<u32>,
 }
 
+impl <T>Cacher<T> 
+where
+    T: Fn(u32) -> u32,
+{
+
+    fn new(calculation_function: T) -> Cacher<T>{
+        Cacher {
+            calculation: calculation_function,
+            value: None,
+        }
+    }
+
+    fn get_value(&mut self, arg: u32) -> u32{
+        match self.value {
+            Some(v) => v,    
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }    
+        } 
+    }
+}
+
 fn main(){
     let simulated_intensity = 22;
     let simulated_random_number = 3;
-
+    
     generate_workout(simulated_intensity, simulated_random_number);
 }
 
-fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+fn generate_workout(mut intensity: u32, random_number: u32) {
+    let mut expensive_closure = Cacher::new(|num| {
         println!("Calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
+
 
     if intensity < 25{
         println!(
             "Today, do {} pushups",
-            expensive_closure(intensity)
+            expensive_closure.get_value(intensity)
         );
+        // intensity +=1;
         println!(
             "Next, do {} situps",
-            expensive_closure(intensity)
+            expensive_closure.get_value(intensity)
         );
     } else {
         // This is not ideal because the expensive calculation will be called even in this case
@@ -45,7 +71,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes",
-                expensive_closure(intensity)
+                expensive_closure.get_value(intensity)
             );
         }
     }
