255d7e339a7f0a091d04d0a479b5fd69032944cf
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-27 14:06:09 +1200

    Use closure instead of function

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,36 @@ fn simulated_expensive_calculation(intensity: u32) -> u32{
     intensity
 }
 
+struct Cacher<T>
+where 
+    T: Fn(u32)-> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
 fn main(){
-    let simulated_intensity = 26;
+    let simulated_intensity = 22;
     let simulated_random_number = 3;
 
     generate_workout(simulated_intensity, simulated_random_number);
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    let expensive_closure = |num| {
+        println!("Calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
 
     if intensity < 25{
         println!(
             "Today, do {} pushups",
-            expensive_result
+            expensive_closure(intensity)
         );
         println!(
             "Next, do {} situps",
-            expensive_result
+            expensive_closure(intensity)
         );
     } else {
         // This is not ideal because the expensive calculation will be called even in this case
@@ -33,7 +45,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes",
-                expensive_result
+                expensive_closure(intensity)
             );
         }
     }
